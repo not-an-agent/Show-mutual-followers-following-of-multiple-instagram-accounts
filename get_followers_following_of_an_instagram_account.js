c492c1375c5a483219795b85eae7cd47cb67a118
not-an-agent <140798900+not-an-agent@users.noreply.github.com>
2023-12-31 19:34:37 +0200

    Update get_followers_following_of_an_instagram_account.js
    added followers / following count to console / text file / filename

--- get_followers_following_of_an_instagram_account.js
+++ get_followers_following_of_an_instagram_account.js
@@ -3,19 +3,23 @@
 // or:
 // Network conditions > Chrome - Android Mobile (high-end) > Mozilla/5.0 (Linux; Android 10; Pixel 4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36
 
-let spans = document.querySelectorAll('span');
-let firstSpanWithoutChildAndSiblings;
+(async () => {
+  try {
+    // Select all span elements
+    let spans = document.querySelectorAll('span');
+    let firstSpanWithoutChildAndSiblings;
 
-try {
+    // Find the first span that meets the specified conditions
     for (let span of spans) {
-        if (span.parentNode.tagName === 'DIV' && span.childElementCount === 0 && span.parentNode.childElementCount === 1) {
-            firstSpanWithoutChildAndSiblings = span;
-            break;
-        }
+      if (span.parentNode.tagName === 'DIV' && span.childElementCount === 0 && span.parentNode.childElementCount === 1) {
+        firstSpanWithoutChildAndSiblings = span;
+        break;
+      }
     }
 
+    // Check if a matching span is found
     if (!firstSpanWithoutChildAndSiblings) {
-        throw new Error('No matching span found.');
+      throw new Error('No matching span found.');
     }
 
     // Get the class of the span
@@ -32,45 +36,75 @@ try {
 
     // Check if there are no elements with the specified class
     if (elements.length === 0) {
-        throw new Error('No elements found with the specified class.');
+      throw new Error('No elements found with the specified class.');
     }
 
     // Create an array to store the values
     const values = [];
     const currentURL = window.location.href;
     const followers_following = currentURL.split("/")[4];
-    const filename = currentURL.split("/")[3] + '-' + currentURL.split("/")[4];
 
-    values.push('Account ' + followers_following + ':\n' + currentURL + '\n');
+    // Input Instagram username
+    const username = currentURL.split("/")[3];
 
-    // Loop through the elements and store their values
-    elements.forEach(element => {
+    // Construct Instagram profile URL
+    const url = `https://www.instagram.com/${username}/`;
+
+    // Make request to page
+    const response = await fetch(url);
+    const html = await response.text();
+
+    // Parse HTML to find meta tag with follower count
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, 'text/html');
+    const meta = doc.querySelector('meta[name="description"]');
+
+    // Check if the meta tag is found
+    if (meta) {
+      let content = meta.getAttribute('content');
+      console.log(content);
+
+      // Extract follower count from content string
+      content = content.split(" ");
+      console.log(content);
+      console.log("Followers: " + content[0]);
+      console.log("Following: " + content[2]);
+
+      const total_followers_following = followers_following == "followers" ? content[0] : content[2];
+
+      values.push('Account ' + followers_following + ':\n' + currentURL + '\n');
+      values.push(`${followers_following[0].toUpperCase() + followers_following.slice(1)} count: ${elements.length} / ${total_followers_following}\n`);
+
+      // Loop through the elements and store their values
+      elements.forEach(element => {
         values.push("https://www.instagram.com/" + element.textContent);
         console.log("https://www.instagram.com/" + element.textContent);
-    });
+      });
 
-    // Print the count of elements
-    console.log('Count:', elements.length);
+      // Print the count of elements
+      console.log('Count: ' + elements.length + " / " + total_followers_following);
 
-    console.log('Account ' + followers_following + ':\n' + currentURL + '\n')
+      console.log('Account ' + followers_following + ':\n' + currentURL + '\n');
 
-    // Create a Blob with the values
-    const blob = new Blob([values.join('\n')], {
+      // Create a Blob with the values
+      const blob = new Blob([values.join('\n')], {
         type: 'text/plain'
-    });
+      });
 
-    // Remove any existing button with id 'download_file'
-    const existingButton = document.getElementById('download_file');
-    if (existingButton) {
+      // Remove any existing button with id 'download_file'
+      const existingButton = document.getElementById('download_file');
+      if (existingButton) {
         existingButton.parentNode.removeChild(existingButton);
-    }
+      }
 
-    // Create a link (button) to download the Blob
-    const link = document.createElement('button');
-    link.id = 'download_file';
-    link.style.display = 'block';
-    link.textContent = 'Download the file';
-    link.onclick = function (e) {
+      const filename = currentURL.split("/")[3] + "-" + currentURL.split("/")[4] + "-" + elements.length + "_" + total_followers_following;
+
+      // Create a link (button) to download the Blob
+      const link = document.createElement('button');
+      link.id = 'download_file';
+      link.style.display = 'block';
+      link.textContent = 'Download the file';
+      link.onclick = function (e) {
         const url = URL.createObjectURL(blob);
         const a = document.createElement('a');
         a.style.display = 'none';
@@ -80,19 +114,25 @@ try {
         a.click();
         document.body.removeChild(a);
         URL.revokeObjectURL(url);
-        e.preventDefault(); // prevent button default behavior
-    };
+        e.preventDefault();
+        // prevent button default behavior
+      };
 
-    // Get the parent div
-    const parentDiv = document.querySelector('div[style="display: flex; flex-direction: column; padding-bottom: 0px; padding-top: 0px; position: relative;"]');
+      // Get the parent div
+      const parentDiv = document.querySelector('div[style="display: flex; flex-direction: column; padding-bottom: 0px; padding-top: 0px; position: relative;"]');
 
-    if (!parentDiv) {
+      // Check if the parent div is found
+      if (!parentDiv) {
         throw new Error('Parent div not found.');
+      }
+
+      // Append the link (button) to the parent div
+      parentDiv.append(link);
+
+    } else {
+      throw new Error('Meta tag not found.');
     }
-
-    // Append the link (button) to the parent div
-    parentDiv.append(link);
-
-} catch (error) {
+  } catch (error) {
     console.error('Error:', error.message);
-}
+  }
+})();
